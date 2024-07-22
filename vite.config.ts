3082b9df28339b32384be9f9398b4c15c0e89dc5
Jerry <x.jerry.wang@gmail.com>
2024-07-22 09:08:54 +0000

    chore: clean deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import path from 'path'
 import Pages from 'vite-plugin-pages'
-import Components from 'unplugin-vue-components/vite'
 import Unocss from 'unocss/vite'
 import { DocsBlock } from './vite/DocsBlock'
 
@@ -20,11 +19,6 @@ export default defineConfig(({}) => {
       Vue(),
 
       DocsBlock(),
-
-      // https://github.com/antfu/vite-plugin-components
-      Components({
-        dts: 'src/auto-components.d.ts',
-      }),
 
       // https://github.com/hannoeru/vite-plugin-pages
       Pages({
